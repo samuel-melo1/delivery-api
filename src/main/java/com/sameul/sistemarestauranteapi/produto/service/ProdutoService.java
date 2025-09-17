1e69504e01af48d56a3b8f5c284f4cbc4faa2802
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-09-16 22:41:49 -0300

    feat: adiciona método de salvar e buscar produto por id

--- src/main/java/com/sameul/sistemarestauranteapi/produto/service/ProdutoService.java
+++ src/main/java/com/sameul/sistemarestauranteapi/produto/service/ProdutoService.java
@@ -1,8 +1,12 @@
 package com.sameul.sistemarestauranteapi.produto.service;
 
-import com.sameul.sistemarestauranteapi.produto.controller.request.ProdutoRequest;
-import com.sameul.sistemarestauranteapi.produto.service.response.ProdutoResponse;
+import com.sameul.sistemarestauranteapi.produto.dto.request.ProdutoRequest;
+import com.sameul.sistemarestauranteapi.produto.dto.response.ProdutoResponse;
+import org.springframework.data.domain.Page;
 
 public interface ProdutoService {
     ProdutoResponse salvar(ProdutoRequest request);
+    ProdutoResponse buscarProdutoPorId(Integer id);
+
+    Page<ProdutoResponse> listarProdutos(int page, int size); 
 }
--- src/main/java/com/sameul/sistemarestauranteapi/produto/service/impl/ProdutoServiceImpl.java
+++ src/main/java/com/sameul/sistemarestauranteapi/produto/service/impl/ProdutoServiceImpl.java
@@ -1,17 +1,48 @@
 package com.sameul.sistemarestauranteapi.produto.service.impl;
 
-import com.sameul.sistemarestauranteapi.produto.controller.request.ProdutoRequest;
+import com.sameul.sistemarestauranteapi.common.exceptions.ObjectNotFoundException;
+import com.sameul.sistemarestauranteapi.produto.dto.request.ProdutoRequest;
+import com.sameul.sistemarestauranteapi.produto.dto.response.ProdutoResponse;
+import com.sameul.sistemarestauranteapi.produto.entity.Produto;
+import com.sameul.sistemarestauranteapi.produto.enums.ProdutoErrorCode;
+import com.sameul.sistemarestauranteapi.produto.enums.ProdutoStatus;
+import com.sameul.sistemarestauranteapi.produto.mapper.ProdutoMapper;
+import com.sameul.sistemarestauranteapi.produto.repository.ProdutoRepository;
 import com.sameul.sistemarestauranteapi.produto.service.ProdutoService;
-import com.sameul.sistemarestauranteapi.produto.service.response.ProdutoResponse;
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProdutoServiceImpl  implements ProdutoService {
+@Slf4j
+@RequiredArgsConstructor
+public class ProdutoServiceImpl implements ProdutoService {
+
+    private final ProdutoRepository repository;
+    private final ProdutoMapper mapper;
 
     @Override
     @Transactional
     public ProdutoResponse salvar(ProdutoRequest request) {
+        log.info("Iniciando processo para salvar um novo produto.");
+        Produto produto = mapper.requestToEntity(request);
+        produto.setStatus(ProdutoStatus.ATIVO);
+
+        Produto produtoSalvo = repository.save(produto);
+        log.info("Produto salvo com sucesso com ID: {}", produtoSalvo.getId());
+        return mapper.produtoToResponse(produtoSalvo);
+    }
+    @Override
+    public ProdutoResponse buscarProdutoPorId(Integer id){
+         Produto produto = repository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(ProdutoErrorCode.PRODUTO_NAO_ENCONTRADO));
+         return mapper.produtoToResponse(produto);
+    }
+
+    @Override
+    public Page<ProdutoResponse> listarProdutos(int page, int size) {
         return null;
     }
 }
