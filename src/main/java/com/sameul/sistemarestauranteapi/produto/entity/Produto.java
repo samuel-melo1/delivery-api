c60e074a9e498b71b1c0ffa560df7470c37901b5
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-09-16 22:42:49 -0300

    feat: adicionado builder, construtores, getters e setter

--- src/main/java/com/sameul/sistemarestauranteapi/produto/entity/Produto.java
+++ src/main/java/com/sameul/sistemarestauranteapi/produto/entity/Produto.java
@@ -1,25 +1,15 @@
 package com.sameul.sistemarestauranteapi.produto.entity;
 
 import com.sameul.sistemarestauranteapi.categoriaproduto.entity.CategoriaProduto;
+import com.sameul.sistemarestauranteapi.produto.enums.ProdutoStatus;
 import com.sameul.sistemarestauranteapi.restaurante.entity.Restaurante;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
 public class Produto implements Serializable {
 
     @Id
@@ -27,12 +17,60 @@ public class Produto implements Serializable {
     private Integer id;
     private String descricao;
     private Float preco;
-    private Integer status;
+    @Enumerated(EnumType.STRING)
+    private ProdutoStatus status;
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private CategoriaProduto categoria;
     @ManyToOne
     @JoinColumn(name = "restaurante_id")
     private Restaurante restaurante;
-
+    public Produto(String descricao, Float preco) {
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+    public Produto(Integer id, String descricao, Float preco, ProdutoStatus status, CategoriaProduto categoria, Restaurante restaurante) {
+        this.id = id;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.status = status;
+        this.categoria = categoria;
+        this.restaurante = restaurante;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public Float getPreco() {
+        return preco;
+    }
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+    public ProdutoStatus getStatus() {
+        return status;
+    }
+    public void setStatus(ProdutoStatus status) {
+        this.status = status;
+    }
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(CategoriaProduto categoria) {
+        this.categoria = categoria;
+    }
+    public Restaurante getRestaurante() {
+        return restaurante;
+    }
+    public void setRestaurante(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
 }
