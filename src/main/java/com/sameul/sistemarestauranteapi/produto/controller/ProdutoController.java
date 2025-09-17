d1c751f76c028c320d6286b53ed994476367f163
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-09-16 22:45:06 -0300

    feat: adiciona controller com método de salvar do produto

--- src/main/java/com/sameul/sistemarestauranteapi/produto/controller/ProdutoController.java
+++ src/main/java/com/sameul/sistemarestauranteapi/produto/controller/ProdutoController.java
@@ -1,9 +1,25 @@
 package com.sameul.sistemarestauranteapi.produto.controller;
 
+import com.sameul.sistemarestauranteapi.produto.dto.request.ProdutoRequest;
+import com.sameul.sistemarestauranteapi.produto.dto.response.ProdutoResponse;
+import com.sameul.sistemarestauranteapi.produto.service.ProdutoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
+
+    private final ProdutoService service;
+    public ProdutoController(ProdutoService service){
+        this.service = service;
+    }
+    @PostMapping
+    public ResponseEntity<ProdutoResponse> salvar(@RequestBody @Valid ProdutoRequest request){
+        return ResponseEntity.ok(service.salvar(request));
+    }
 }
