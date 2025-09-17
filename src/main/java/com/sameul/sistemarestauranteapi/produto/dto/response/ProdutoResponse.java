a31846e4a067553dcdb394294edb35881407f97d
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-09-16 22:40:33 -0300

    feat: adiciona um dto de response para o produto

---
+++ src/main/java/com/sameul/sistemarestauranteapi/produto/dto/response/ProdutoResponse.java
@@ -0,0 +1,34 @@
+package com.sameul.sistemarestauranteapi.produto.dto.response;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class ProdutoResponse {
+
+    private Integer id;
+    private String descricao;
+    private Float preco;
+
+    public ProdutoResponse(){}
+    public ProdutoResponse(String descricao, Float preco) {
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public Float getPreco() {
+        return preco;
+    }
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
