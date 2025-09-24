79204b68c97f569f83cc9c7eb1f70a6becb28623
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-09-23 23:08:31 -0300

    feat: adiciona controller para salvar produto e deletar produto

--- src/main/java/com/sameul/sistemarestauranteapi/produto/controller/ProdutoController.java
+++ src/main/java/com/sameul/sistemarestauranteapi/produto/controller/ProdutoController.java
@@ -3,12 +3,16 @@ package com.sameul.sistemarestauranteapi.produto.controller;
 import com.sameul.sistemarestauranteapi.produto.dto.request.ProdutoRequest;
 import com.sameul.sistemarestauranteapi.produto.dto.response.ProdutoResponse;
 import com.sameul.sistemarestauranteapi.produto.service.ProdutoService;
+import com.sameul.sistemarestauranteapi.restaurante.enums.RestauranteStatus;
+import com.sameul.sistemarestauranteapi.restaurante.request.RestauranteRequest;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/produtos")
@@ -20,6 +24,17 @@ public class ProdutoController {
     }
     @PostMapping
     public ResponseEntity<ProdutoResponse> salvar(@RequestBody @Valid ProdutoRequest request){
-        return ResponseEntity.ok(service.salvar(request));
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().build().toUri();
+        return ResponseEntity.created(uri).body(service.salvar(request));
+    }
+    @GetMapping
+    public ResponseEntity<Page<ProdutoResponse>> listar(@RequestParam(defaultValue = "0") Integer page,
+                                                                   @RequestParam(defaultValue = "10") Integer size){
+        return ResponseEntity.status(HttpStatus.OK).body(service.listar(page, size));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") Integer id){
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 }
