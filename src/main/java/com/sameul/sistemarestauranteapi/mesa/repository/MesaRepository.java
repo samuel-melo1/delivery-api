2b75d04ece312af9de8daaf2ce6d504bb1486ae2
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-10-20 21:44:24 -0300

    feat: criado query por numero da mesa

--- src/main/java/com/sameul/sistemarestauranteapi/mesa/repository/MesaRepository.java
+++ src/main/java/com/sameul/sistemarestauranteapi/mesa/repository/MesaRepository.java
@@ -3,5 +3,9 @@ package com.sameul.sistemarestauranteapi.mesa.repository;
 import com.sameul.sistemarestauranteapi.mesa.entity.Mesa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MesaRepository extends JpaRepository<Mesa, Integer> {
+
+    Optional<Mesa> findMesaByNumeroMesa(Integer numeroMesa);
 }
